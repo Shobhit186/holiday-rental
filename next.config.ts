d4af8d18083f9babf08a65af209477dff9b6e65c
Shobhit Dwivedi <Shobhit.Dwivedi@gds.ey.com>
2025-02-17 09:56:13 +0530

    revert

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true, // Disable TypeScript build errors
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // Disable ESLint during build
-  },
   /* config options here */
   images: {
     domains: ["lh3.googleusercontent.com","avatars.githubusercontent.com","res.cloudinary.com"],
